2c0d2801237c6495e41987712697e83588d371b7
Alehandro <shohin-sasha1993@yandex.ru>
2024-10-16 21:05:11 +0500

    Add Metods in UserController

--- MonsterRPG.API/Controllers/UserComtroller.cs
+++ MonsterRPG.API/Controllers/UserComtroller.cs
@@ -6,9 +6,28 @@ namespace MonsterRPG.API.Controllers
     [Route("api/[controller]")] //api приписка служит для отличия урлы фронта от бека
     public class UserComtroller : Controller
     {
-        public IActionResult Index()
+        [HttpPost]
+        public ActionResult AddUser()
         {
-            return View();
+            return Ok("Успешно добавлен");
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteUser(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet]
+        public ActionResult GetUserById(int id)
+        {
+            //Если не нашли то
+            return NotFound($"User{id} was noy found");
+
+            //если нашли то
+
+            //return Ok(new User());
         }
     }
+
 }
